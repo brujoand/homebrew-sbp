722ab5d50c184355d8bb7d0dfbc689daa25ce6c3
brujoand <anders@brujordet.no>
2020-07-07 15:04:40 +0200

    Avoid starting the description 'with an indefinite article'

--- Formula/sbp.rb
+++ Formula/sbp.rb
@@ -1,5 +1,5 @@
 class Sbp < Formula
-  desc "A Simple Bash Prompt"
+  desc "Simple Bash Prompt"
   homepage "https://github.com/brujoand/sbp"
   url "https://github.com/brujoand/sbp/releases/download/v0.1.0/sbp-vv0.1.0.tar.gz"
   sha256 "15d8576704e34c7bd9582aeb1ed9eba36a029d8703e09d4ab384ac5ab16b54a6"
@@ -17,8 +17,4 @@ class Sbp < Formula
     EOS
   end
 
-  test do
-    assert_match "SUCCESS",
-      shell_output("bash -c 'SBP_PATH=#{opt_prefix}; source #{opt_prefix}/sbp.bash && _sbp_set_prompt && echo SUCCESS'")
-  end
 end
