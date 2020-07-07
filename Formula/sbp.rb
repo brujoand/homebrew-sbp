1b2f7976b0b1c25ef337317da5f09deccb9375b7
brujoand <anders@brujordet.no>
2020-07-07 13:55:52 +0200

    Initial commit

---
+++ Formula/sbp.rb
@@ -0,0 +1,24 @@
+class Sbp < Formula
+  desc "A Simple Bash Prompt"
+  homepage "https://github.com/brujoand/sbp"
+  url "https://github.com/brujoand/sbp/archive/sbp-vv0.1.0.tar.gz"
+  sha256 "15d8576704e34c7bd9582aeb1ed9eba36a029d8703e09d4ab384ac5ab16b54a6"
+
+  depends_on "bash"
+
+  def caveats
+    <<~EOS
+      To activate sbp, add the following line to ${HOME}/.bashrc:
+
+        SBP_PATH="#{opt_prefix}"
+        source "${SBP_PATH}/sbp.bash"
+
+      Alternatively just execute "#{opt_prefix}/bin/install".
+    EOS
+  end
+
+  test do
+    assert_match "SUCCESS",
+      shell_output("bash -c 'SBP_PATH=#{opt_prefix}; source #{opt_prefix}/sbp.bash && _sbp_set_prompt && echo SUCCESS'")
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# homebrew-sbp
-SBP Homebrew Tap
+# SBP Homebrew Tap
+
+## How do I install SBP?
+`brew install brujoand/sbp/sbp`
+
+Or `brew tap brujoand/sbp` and then `brew install sbp`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+
+## Update SBP with Homebrew
+
+```bash
+brew update && brew upgrade
+```
+
+## Uninstall SBP with Homebrew
+
+```bash
+brew uninstall brujoand/sbp/sbp
+```
